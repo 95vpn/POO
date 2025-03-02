1775d67a06edcdc49bb7ea53ec98e898ff23d1d0
Johnny Herrera <jmjaramillo95@gmail.com>
2025-03-01 23:53:27 -0500

    Agregar archivos de proyecto.

---
+++ POO/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+Beer erdingerBeer = new Beer()
+{
+    Name = "Erdinger",
+    Price = 3
+};
+
+Console.WriteLine(erdingerBeer.Name);
+
+public class Beer
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+}
